f07eeeb5e249cfc83e4ca37d467e130d3344481f
Akanica <228978180+Lote222@users.noreply.github.com>
2025-09-01 02:25:25 -0500

    ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'La Balota Mágica',
+  title: 'La Balota de la Fortuna',
   description: 'Consulta los resultados del sorteo y mantente al día.',
 };
 
